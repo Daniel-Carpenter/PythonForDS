1585f1333fb89509dae922b1852f24d7d240f3c3
Daniel-Carpenter <dbry1024@gmail.com>
2022-05-17 10:05:23 -0500

    Notes

--- 02 - Notes and Assignments/02 - Functions, Methods, and Loops/03 - Read and Write Files/README.md
+++ 02 - Notes and Assignments/02 - Functions, Methods, and Loops/03 - Read and Write Files/README.md
@@ -7,3 +7,52 @@
 * Python attributes
 * `txt` files
 * `Excel` files
+
+---
+
+<br>
+
+# Example of Reading and writing to files
+> This script provides two simple examples of writing and reading files
+
+## Open a file that already exists and write 500 random numbers
+
+
+```python
+file_loc = "C:/Users/daniel.carpenter/OneDrive - the Chickasaw Nation/Documents/GitHub/OU-DSA/Python for DSA/02 - Notes and Assignments/02 - Functions, Methods, and Loops/03 - Read and Write Files/theOutputFile.txt"
+
+import random
+
+# Open the files
+f = open(file_loc, 'w')
+
+# 500 lines to write to file
+for count in range(500):
+    
+    # Make a random number and generate random number
+    number = random.randint(1, 500)
+    f.write(str(number) + '\n')
+
+f.close() # Close the output
+```
+
+## Read the numbers in the created file, then sum up the numbers and print
+
+
+```python
+f = open(file_loc, 'r')
+theSum = 0
+
+# For each line in the file
+for line in f:
+    line = line.strip() # trim the whitespace of each line
+    number = int(line)  # Read as coerced int
+    theSum += number    # Cumulative sum
+
+f.close() # close the file
+
+print("The sum of numbers from the created file is", theSum)
+```
+
+    The sum of numbers from the created file is 123125
+    
--- 02 - Notes and Assignments/02 - Functions, Methods, and Loops/03 - Read and Write Files/Read_Write_to_Files.py
+++ 02 - Notes and Assignments/02 - Functions, Methods, and Loops/03 - Read and Write Files/Read_Write_to_Files.py
@@ -1,31 +1,41 @@
 # Week 2 - Files
 # This script provides two simple examples of writing and reading files
 
-# This block will open a file that already exists and write 500
-# random numbers
+"""
+This block will open a file that already exists and write 500
+random numbers
+"""
 
-file_loc = "C:\\DSA_OU\\boomerNumbers.txt"
+file_loc = "C:/Users/daniel.carpenter/OneDrive - the Chickasaw Nation/Documents/GitHub/OU-DSA/Python for DSA/02 - Notes and Assignments/02 - Functions, Methods, and Loops/03 - Read and Write Files/theOutputFile.txt"
 
 import random
+
+# Open the files
 f = open(file_loc, 'w')
+
+# 500 lines to write to file
 for count in range(500):
+    
+    # Make a random number and generate random number
     number = random.randint(1, 500)
     f.write(str(number) + '\n')
-f.close()
+
+f.close() # Close the output
 
 
-# Now lets read the file but 1st
-# go back and save some typing
-
+"""
+Read the numbers in the created file
+Sum up the numbers and print
+"""
 f = open(file_loc, 'r')
 theSum = 0
+
+# For each line in the file
 for line in f:
-    line = line.strip()
-    number = int(line)
-    theSum += number
+    line = line.strip() # trim the whitespace of each line
+    number = int(line)  # Read as coerced int
+    theSum += number    # Cumulative sum
 
-f.close()
+f.close() # close the file
 
-print("The sum is", theSum)
-
-
+print("The sum of numbers from the created file is", theSum)
